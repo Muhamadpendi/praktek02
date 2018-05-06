1b740f4f82981c59deae95c7698ac45c05a8ce42
Muhamad Pendi <muhamadpendi52@gmail.com>
2018-05-06 20:20:52 +0800

    Mengisi nilai attribut

--- src/praktek02/TriangleAksi.java
+++ src/praktek02/TriangleAksi.java
@@ -3,5 +3,8 @@ package praktek02;
 public class TriangleAksi {
     public static void main(String[] args) {
         Triangle r1 = new Triangle();
+        r1.alas = 8;
+        r1.tinggi = 4;
+        
     }
 }
